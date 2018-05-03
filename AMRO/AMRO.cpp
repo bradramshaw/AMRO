b325dd1429347009e6a38c41f2270f8462b0b712
bradramshaw <bradramshaw@gmail.com>
2018-05-03 16:37:53 -0400

    Finished vy, vz
    done

--- AMRO/AMRO.cpp
+++ AMRO/AMRO.cpp
@@ -84,8 +84,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	argx[0] = 1;
 	argy[0] = 2;
 	argz[0] = 1;
-	veloY(params, argx, argy, argz, 1, tempy, vy);
-	cout << vy[0] << endl;
+	veloZ(params, argx, argy, argz, 1, tempz, vz);
+	cout << vz[0] << endl;
 
 	for (int th = 0; th < 46; th++) {
 
@@ -339,18 +339,35 @@ int veloY(Ipp64f *params, Ipp64f *kx, Ipp64f *ky, Ipp64f *kz, int length, Ipp64f
 	ippsMulC_64f_I(65893 * 4, &temp[10 * length], length); // mult by constant
 	ippsSub_64f(&temp[4 * length], &temp[2 * length], &temp[11 * length], length);//cos kx - cos ky
 	ippsMulC_64f(&temp[11 * length], 65893, &temp[12 * length], length);//mult by constant
-	ippsMul_64f(&temp[5 * length], &temp[12 * length], &temp[13 * length], length);// mult by sin kx/2
-	ippsAdd_64f(&temp[13 * length], &temp[10 * length], temp, length);//add those two together
+	ippsMul_64f(&temp[7 * length], &temp[12 * length], &temp[13 * length], length);// mult by sin kx/2
+	ippsSub_64f(&temp[10 * length], &temp[13 * length], temp, length);//add those two together (neg sign)	
 	ippsMul_64f_I(&temp[9 * length], temp, length);//mult by cos kz/2
 	ippsMul_64f_I(&temp[11 * length], temp, length);//mult by cos kx - cos ky
-	ippsMul_64f_I(&temp[8 * length], temp, length);//mult by cos ky/2
+	ippsMul_64f_I(&temp[6 * length], temp, length);//mult by cos kx/2	
 	ippsMulC_64f_I(params[6 - 1] * 0.0000866667, temp, length);
 	ippsAdd_64f_I(temp, out, length);
 	return 0;
 }
 
 int veloZ(Ipp64f *params, Ipp64f *kx, Ipp64f *ky, Ipp64f *kz, int length, Ipp64f *temp, Ipp64f *out) {
-	
+	ippsMulC_64f(kx, 3.74767, temp, length); //term for long complicated kz term, param6
+	vdCos(length, temp, &temp[2 * length]); // cos kx
+	ippsMulC_64f(ky, 3.74767, temp, length);
+	vdCos(length, temp, &temp[4 * length]); // cos ky
+	ippsMulC_64f(kx, 3.74767 / 2, temp, length);
+	vdCos(length, temp, &temp[6 * length]); // cos kx/2
+	ippsMulC_64f(ky, 3.74767 / 2, temp, length);
+	vdCos(length, temp, &temp[8 * length]); // cos ky/2
+	ippsMulC_64f(kz, 3.3, temp, length);
+	vdSin(length, temp, &temp[9 * length]); // sin kz/2
+	ippsSub_64f(&temp[4 * length], &temp[2 * length], &temp[11 * length], length);//cos kx - cos ky
+	ippsSqr_64f_I(&temp[11 * length], length);//square it
+	ippsMul_64f_I(&temp[9 * length], &temp[11 * length], length);// times sin kz/2
+	ippsMul_64f_I(&temp[8 * length], &temp[11 * length], length);// times cos ky/2
+	ippsMul_64f_I(&temp[6 * length], &temp[11 * length], length);// times cos ky/2
+	ippsMulC_64f(&temp[11 * length],params[6 - 1] * 10.0571,out, length);
+
+
 	return 0;
 }
 
